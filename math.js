e134aedb25a76fe4094706ebee3c15e36c607244
Pavel.I <ivannikov.pavel1@mail.ru>
2022-09-26 00:16:37 +0300

    initial commit

---
+++ math.js
@@ -0,0 +1,58 @@
+/**
+ * Функция вычисления суммы чисел
+ * @param {Number} x - Первое слагаемое
+ * @param {Number} y - Второе слагаемое
+ * @returns Сумма x и y
+ */
+function add(x, y) {
+    return x + y;
+}
+
+/**
+ * Функция вычисления разности чисел
+ * @param {Number} x - Уменьшаемое
+ * @param {Number} y - Вычитаемое
+ * @returns Разность x и y
+ */
+function sub(x, y) {
+    return x - y;
+}
+
+/**
+ * Функция вычисления произведения чисел
+ * @param {Number} x - Первый множитель
+ * @param {Number} y - Второй множитель
+ * @returns Произведение x на y
+ */
+function mul(x, y) {
+    return x * y;
+}
+
+/**
+ * Функция вычисления произведения чисел
+ * @param {Number} x - Первый множитель
+ * @param {Number} y - Второй множитель
+ * @returns Произведение x на y
+ */
+function div(x, y) {
+    return x / y;
+}
+
+/**
+ * Функция возведения числа в степень
+ * @param {Number} x - Основание степени
+ * @param {Number} y - Показатель степени
+ * @returns Значение x в степени y
+ */
+function pow(x, y) {
+        return Math.pow(x,y);
+}
+
+/**
+ * Функция вычисления квадратного корня
+ * @param {Number} x - Подкоренное выражение (число)
+ * @returns Квадратный корень из x
+ */
+function sqrt(x) {
+    return Math.sqrt(x);
+}
---
+++ script.js
@@ -0,0 +1,167 @@
+var first = undefined;
+var second = undefined;
+
+var addition = false;
+var substraction = false;
+var multiplication = false;
+var division = false;
+
+/**
+ * Проверка, нажата ли кнопка сложения, вычитания, умножения или деления
+ * @returns true ели нажата одна из кнопок
+ */
+function anyOperationApplyed() {
+    return addition | substraction | multiplication | division;
+}
+
+/**
+ * С, умножения или деления
+ */
+function clearOperations() {
+    first = undefined;
+    second = undefined;
+    addition = false;
+    substraction = false;
+    multiplication = false;
+    division = false;
+}
+
+/**
+ * Нажатие на кнопку сложения
+ */
+function setAddition() {
+    clearOperations();
+    addition = true;
+    first = getOutput();
+}
+
+/**
+ * Нажатие на кнопку вычитания
+ */
+function setSubstraction() {
+    clearOperations();
+    substraction = true;
+    first = getOutput();
+}
+
+/**
+ * Нажатие на кнопку умножения
+ */
+function setMultiplication() {
+    clearOperations();
+    multiplication = true;
+    first = getOutput();
+}
+
+/**
+ * Нажатие на кнопку деления
+ */
+function setDivision() {
+    clearOperations();
+    division = true;
+    first = getOutput();
+}
+
+/**
+ * Нажатие на кномку равно, т.е. вычисление результата
+ */
+function countResult() {
+    if (first) {
+        second = getOutput();
+        var result = undefined;
+        if (addition) {
+            result = add(first, second);
+        } else if (substraction) {
+            result = sub(first, second);
+        } else if (multiplication) {
+            result = mul(first, second)
+        } else if (division) {
+            result = div(first, second);
+        }
+        first = result;
+        second = undefined;
+        setOutput(result);
+    }
+}
+
+/**
+ * Получить число на экаране калькулятора
+ * @returns Объект Number со значением
+ */
+function getOutput() 
+{
+    let text = document.getElementById("output").innerText;
+    return Number(text);
+}
+
+/**
+ * Установить значение на экране калькулятора
+ * @param {Any} value 
+ */
+function setOutput(value) 
+{
+    document.getElementById("output").innerText = value;
+    
+    
+}
+
+/**
+ * Нажатие на кнопку 0-9
+ * @param {Number} value 
+ */
+function numberButtonClick(value) 
+{
+    if (anyOperationApplyed()) 
+    {
+        setOutput(0);
+    }
+
+    const out = getOutput();
+
+    if (out.toString().length < 8) 
+    {
+        if (out === 0) 
+        {
+            setOutput(value);
+        } 
+        else 
+        {
+            setOutput(out.toString() + value.toString());
+        }
+    }
+}
+
+/**
+ * Нажатие на кнопку вычисления квадратного корня
+ */
+function root() {
+    setOutput(sqrt(getOutput()));
+}
+
+/**
+ * Нажатие на кнопку возведения в квадрат
+ */
+function squared() {
+    setOutput(pow(getOutput(), 2));
+}
+
+/**
+ * Нажатие на кнопку смены знака числа
+ */
+function switchSign() {
+    setOutput(-getOutput());
+}
+
+/**
+ * Нажатие на кнопку получение обратного числа
+ */
+function reverseNumber() {
+    setOutput(div(1, getOutput()));
+}
+
+/**
+ * Нажатие на кнопку CE
+ */
+function dropAll() {
+    setOutput(0);
+}
